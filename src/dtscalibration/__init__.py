ffb8f5caa41ab2a101be04272ca299ca06f879d6
Bas des Tombe <bdestombe@gmail.com>
2018-08-05 12:08:38 +0200

    sort imports init datastore

--- src/dtscalibration/__init__.py
+++ src/dtscalibration/__init__.py
@@ -1,5 +1,6 @@
 # coding=utf-8
-from .datastore import DataStore, open_datastore
+from .datastore import DataStore
+from .datastore import open_datastore
 
 __version__ = '0.1.1'
 __all__ = ["DataStore", "open_datastore"]
