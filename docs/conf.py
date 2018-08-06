c6b151ef806ba4e63f797011cbaaca5509396bfc
Bas des Tombe <bdestombe@gmail.com>
2018-08-06 13:43:21 +0200

    Bump version: 0.1.1 → 0.1.2

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ project = 'dtscalibration'
 year = '2018'
 author = 'Bas des Tombe'
 copyright = '{0}, {1}'.format(year, author)
-version = release = '0.1.1'
+version = release = '0.1.2'
 
 pygments_style = 'trac'
 templates_path = ['.']
--- src/dtscalibration/__init__.py
+++ src/dtscalibration/__init__.py
@@ -3,5 +3,5 @@ from .datastore import DataStore
 from .datastore import open_datastore
 from .datastore import read_xml_dir
 
-__version__ = '0.1.1'
+__version__ = '0.1.2'
 __all__ = ["DataStore", "open_datastore", "read_xml_dir"]
