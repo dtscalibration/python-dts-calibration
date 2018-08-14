Fixed bug calculate variance stokes and added synthetic test

src/dtscalibration/datastore.py
@@ -85,9 +85,17 @@ class DataStore(xr.Dataset):
         self.sections = None
         pass
 
-    def variance_stokes(self, st_label, sections=None, use_statsmodels=False):
+    def variance_stokes(self, st_label, sections=None, use_statsmodels=False, suppress_info=False):
         """
-        Calculates the variance between the measurements and a best fit exponential at each section
+        Calculates the variance between the measurements and a best fit exponential at each
+        reference section. This fits a two-parameter exponential to the stokes measurements. The
+        temperature is constant and there are no splices/sharp bends in each reference section.
+        Therefore all signal decrease is due to differential attenuation, which is the same for
+        each reference section. The scale of the exponential does differ per reference section.
+
+        Assumptions: 1) the temperature is the same along a reference section. 2) no sharp bends
+        and splices in the reference sections. 3) Same type of optical cable in each reference
+        section.
 
         Parameters
         ----------
@@ -107,7 +115,7 @@ class DataStore(xr.Dataset):
         if sections:
             self.sections = sections
 
-        assert 'time' in self[st_label].dims
+        assert 'time' in self[st_label].dims and 'x' in self[st_label].dims
 
         nt = self['time'].size
 
@@ -126,15 +134,15 @@ class DataStore(xr.Dataset):
         n_sections = len(len_stretch_list)
         n_locs = sum(len_stretch_list)
 
-        x = da.concatenate(x_list)
-        y = da.concatenate(y_list).compute()
+        x = da.concatenate(x_list)  # coordinates are already in memory
+        y = np.asarray(da.concatenate(y_list))
         w = np.sqrt(y)
 
         data1 = x
         data2 = np.ones(sum(len_stretch_list) * nt)
-        data = da.concatenate([data1, data2]).compute()
+        data = np.concatenate([data1, data2])
 
-        wdata = data * da.hstack((w, w))
+        wdata = data * np.hstack((w, w))
 
         # alpha is the same for all -> one column
         coords1row = np.arange(nt * n_locs)
@@ -145,29 +153,26 @@ class DataStore(xr.Dataset):
         coords2col = np.hstack([
             np.repeat(np.arange(i * nt + 1, (i + 1) * nt + 1), in_locs)
             for i, in_locs in enumerate(len_stretch_list)
-        ])  # C for
+            ])  # C for
         coords = (np.concatenate([coords1row, coords2row]),
                   np.concatenate([coords1col, coords2col]))
 
-        wX = sp.coo_matrix(
-            (wdata, coords),
-            shape=(nt * n_locs, 1 + nt * n_sections),
-            dtype=float,
-            copy=False)
-        X = sp.coo_matrix(
-            (data, coords),
-            shape=(nt * n_locs, 1 + nt * n_sections),
-            dtype=float,
-            copy=False)
+        wX = sp.coo_matrix((wdata, coords),
+                           shape=(nt * n_locs, 1 + nt * n_sections),
+                           dtype=float,
+                           copy=False)
+        X = sp.coo_matrix((data, coords),
+                          shape=(nt * n_locs, 1 + nt * n_sections),
+                          dtype=float,
+                          copy=False)
 
-        lny = da.log(y)
-        wlny = (lny * w)
+        lny = np.log(y)
 
         if use_statsmodels:
             # gives the same answer with statsmodel
             import statsmodels.api as sm
 
-            mod_wls = sm.WLS(lny, X.todense(), weights=w**2)
+            mod_wls = sm.WLS(lny, X.todense(), weights=w ** 2)
             res_wls = mod_wls.fit()
             # print(res_wls.summary())
             a = res_wls.params
@@ -177,11 +182,16 @@ class DataStore(xr.Dataset):
                 ])
             I_est = np.exp(C_expand_to_sec) * np.exp(x * a[0])
             resid = I_est - y
-            var_I = resid.std(ddof=1 + nt * n_sections).compute()
+            var_I = resid.std(ddof=1 + nt * n_sections).compute() ** 2
 
         else:
+            wlny = (lny * w)
+
             p0_est = np.asarray([0.] + nt * n_sections * [8])
-            a = ln.lsqr(wX, wlny, x0=p0_est, show=True, calc_var=False)[0]
+            a = ln.lsqr(wX, wlny,
+                        x0=p0_est,
+                        show=not suppress_info,
+                        calc_var=False)[0]
 
             C_expand_to_sec = np.hstack([
                 np.repeat(a[i * nt + 1:(i + 1) * nt + 1], leni)
@@ -189,14 +199,8 @@ class DataStore(xr.Dataset):
                 ])
             I_est = np.exp(C_expand_to_sec) * np.exp(x * a[0])
             resid = I_est - y
-            var_I = resid.std(ddof=1 + nt * n_sections).compute()
-            print("The variance in", st_label, "is", var_I)
+            var_I = resid.std(ddof=1 + nt * n_sections).compute() ** 2
 
-        # if 0:
-        #     print("The variance in", st_label, "is", var_I)
-        #     import matplotlib.pyplot as plt
-        #     plt.plot(y)
-        #     plt.plot(I_est)
         return var_I, resid
 
     def calibration_single_ended(self,

tests/test_datastore.py
@@ -4,6 +4,7 @@ import os
 import tempfile
 
 import numpy as np
+from scipy import stats
 
 from dtscalibration import DataStore
 from dtscalibration import open_datastore
@@ -131,7 +132,7 @@ def test_read_xml_dir_double_ended():
 
 
 def test_variance_of_measurements_sparse():
-    correct_value = 6.33732530977151
+    correct_value = 40.161692081870584
     filepath = data_dir_double_ended2
     ds = read_xml_dir(filepath,
                       timezone_netcdf='UTC',
@@ -147,8 +148,8 @@ def test_variance_of_measurements_sparse():
     np.testing.assert_almost_equal(I_var, correct_value, decimal=5)
 
 
-def test_variance_of_measurements_statsmodel():
-    correct_value = 6.33732530977151
+def test_variance_of_stokes_statsmodel():
+    correct_var = 40.161692081870584
     filepath = data_dir_double_ended2
     ds = read_xml_dir(filepath,
                       timezone_netcdf='UTC',
@@ -161,4 +162,30 @@ def test_variance_of_measurements_statsmodel():
 
     I_var, _ = ds.variance_stokes(st_label='ST', sections=sections, use_statsmodels=True)
 
-    np.testing.assert_almost_equal(I_var, correct_value, decimal=5)
+    np.testing.assert_almost_equal(I_var, correct_var, decimal=5)
+
+
+def test_variance_of_stokes_synthetic():
+    yvar = 5.
+
+    nx = 50
+    x = np.linspace(0., 20., nx)
+
+    nt = 1000
+    beta = np.linspace(3000, 4000, nt)[None]
+
+    y = beta * np.exp(-0.001 * x[:, None])
+
+    y += stats.norm.rvs(size=y.size,
+                        scale=yvar ** 0.5).reshape(y.shape)
+
+    ds = DataStore({'test_ST': (['x', 'time'], y)},
+                   coords={'x':    x,
+                           'time': range(nt)})
+
+    sections = {'placeholder': [slice(0., 20.), ]}
+    test_ST_var, _ = ds.variance_stokes(st_label='test_ST',
+                                        sections=sections)
+
+    np.testing.assert_almost_equal(test_ST_var, yvar,
+                                   decimal=1)