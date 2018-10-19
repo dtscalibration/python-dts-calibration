3f9fb5d54082ab293d392b68b913d6b5c2170edd
Bas des Tombe <bdestombe@gmail.com>
2018-10-19 17:13:07 +0200

    Bump version: 0.5.0 → 0.5.1

--- docs/conf.py
+++ docs/conf.py
@@ -35,7 +35,7 @@ project = 'dtscalibration'
 year = '2018'
 author = 'Bas des Tombe'
 copyright = '{0}, {1}'.format(year, author)
-version = release = '0.5.0'
+version = release = '0.5.1'
 
 pygments_style = 'trac'
 templates_path = ['.']
--- src/dtscalibration/__init__.py
+++ src/dtscalibration/__init__.py
@@ -4,5 +4,5 @@ from .datastore import open_datastore
 from .datastore import plot_dask
 from .datastore import read_silixa_files
 
-__version__ = '0.5.0'
+__version__ = '0.5.1'
 __all__ = ["DataStore", "open_datastore", "read_silixa_files", "plot_dask"]
