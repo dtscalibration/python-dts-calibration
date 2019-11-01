6533c76a7603b3a967acedfa4a7f6442375a872f
Bas des Tombe <bdestombe@gmail.com>
2019-11-01 12:54:44 +0100

    Pypi requires long_description_content_type='text/markdown'
    Signed-off-by: Bas des Tombe <bdestombe@gmail.com>

--- setup.py
+++ setup.py
@@ -32,6 +32,7 @@ setup(
         re.compile('^.. start-badges.*^.. end-badges', re.M | re.S).sub('', read('README.rst')),
         re.sub(':[a-z]+:`~?(.*?)`', r'``\1``', read('CHANGELOG.rst'))
         ),
+    long_description_content_type='text/markdown',
     author='Bas des Tombe',
     author_email='bdestombe@gmail.com',
     url='https://github.com/bdestombe/python-dts-calibration',
