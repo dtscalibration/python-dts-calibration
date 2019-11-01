df9c9bbc8af554e43ac8c824d5492a13aacb8cfa
Bas des Tombe <bdestombe@gmail.com>
2019-11-01 12:44:37 +0100

    Bump version: 0.6.4 → 0.6.5

--- docs/conf.py
+++ docs/conf.py
@@ -35,7 +35,7 @@ project = 'dtscalibration'
 year = '2018'
 author = 'Bas des Tombe'
 copyright = '{0}, {1}'.format(year, author)
-version = release = '0.6.4'
+version = release = '0.6.5'
 
 pygments_style = 'trac'
 templates_path = ['.']
