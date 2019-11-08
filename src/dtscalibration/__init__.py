7adb0c5a201cd37716189355f7489996b489caa6
BSchilperoort <b.schilperoort@gmail.com>
2019-11-08 15:22:55 +0100

    one more import sorted

--- src/dtscalibration/__init__.py
+++ src/dtscalibration/__init__.py
@@ -3,8 +3,8 @@ from .datastore import DataStore
 from .datastore import open_datastore
 from .datastore import open_mf_datastore
 from .datastore import plot_dask
-from .datastore import read_sensornet_files
 from .datastore import read_apsensing_files
+from .datastore import read_sensornet_files
 from .datastore import read_sensortran_files
 from .datastore import read_silixa_files
 
