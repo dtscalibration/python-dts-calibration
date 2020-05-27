d86b0682d25a056add018e44055f3fc39ad2bc74
Bart Schilperoort <b.schilperoort@gmail.com>
2020-05-27 16:04:16 +0200

    fix in wls_sparse nan check

--- src/dtscalibration/calibrate_utils.py
+++ src/dtscalibration/calibrate_utils.py
@@ -1058,7 +1058,7 @@ def wls_sparse(X, y, w=1., calc_cov=False, verbose=False, x0=None,
     else:
         raise NotImplementedError
 
-    assert not np.any(~np.isfinite(X)), 'Nan/inf in X: check reference' +\
+    assert not np.any(~np.isfinite(X.data)), 'Nan/inf in X: check reference' +\
         ' temperatures?'
     assert not np.any(~np.isfinite(w)), 'Nan/inf in weights'
     assert not np.any(~np.isfinite(y)), 'Nan/inf in observations'
