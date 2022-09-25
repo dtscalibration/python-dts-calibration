86b2762331c770ee53a3a3d42260589b0d38de5c
Bart Schilperoort <b.schilperoort@gmail.com>
2022-09-25 17:29:47 +0200

    Updated setup.py for py37 deprecation

--- setup.py
+++ setup.py
@@ -48,7 +48,7 @@ setup(
     packages=find_packages('src'),
     package_dir={'': 'src'},
     py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
-    python_requires='>= 3.6',  # default dictionary is sorted
+    python_requires='>= 3.8',  # default dictionary is sorted
     include_package_data=True,
     zip_safe=False,
     classifiers=[
